b72196bc5fab203fa9e7066f7ff06c6e61b941c7
Nathanael Sumpter <nsumpter98@gmail.com>
2023-01-23 11:22:01 -0500

    added next/previous page functionality to footer.php

--- STATIC/footer.php
+++ STATIC/footer.php
@@ -6,8 +6,26 @@ function htmlFooter($dateCompleted): void
     echo "
     </div>
     <footer>
-        <p>Date Completed: $dateCompleted</p>
-    </footer>
+        ";
+
+    $current_file = basename($_SERVER['PHP_SELF']);
+    $files = glob('*.php');
+    $current_index = array_search($current_file, $files);
+
+
+    if($current_file && isset($files[$current_index - 1])){
+        echo '<a href="' . $files[$current_index - 1] . '" class="previous">Previous Page</a>';
+    }
+
+    if ($current_index !== false && isset($files[$current_index + 1])) {
+        echo '<a href="' . $files[$current_index + 1] . '" class="next">Next Page</a>';
+    }
+
+
+
+
+
+    echo "</footer>
     </body>
     </html>";
 }
