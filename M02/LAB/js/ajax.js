04fe00ba0304165068fbf13ba6565d5be42ab819
Nathanael Sumpter <nsumpter98@gmail.com>
2023-01-25 13:14:54 -0500

    completed Lab1 using AJAX

---
+++ M02/LAB/js/ajax.js
@@ -0,0 +1,40 @@
+function submitForm(){
+    let formData = new FormData(document.getElementById("form1"));
+    let xhr = new XMLHttpRequest();
+
+
+    let path = (window.location.pathname).split("/");
+    path.pop();
+    path = path.join("/");
+
+    xhr.open("POST", path + "/lab1.php", true);
+
+    console.log(path + "/lab1.php");
+
+
+    xhr.send(formData);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr.responseText);
+
+            document.getElementById("resultsCard").style.display = "block";
+
+            let json = JSON.parse(xhr.responseText);
+
+            document.getElementById("results").innerHTML = "<p>" + json.message + "</p>";
+
+            if(json.pass){
+                document.getElementById("resultsCard").style.backgroundColor = "green";
+            }
+            else{
+                document.getElementById("resultsCard").style.backgroundColor = "red";
+            }
+
+            if(json.status === "error"){
+                document.getElementById("form1").reset();
+            }
+
+        }
+    }
+}
---
+++ M02/LAB/lab1.php
@@ -0,0 +1,59 @@
+<?php
+
+
+function processFormJSON(): void
+{
+    $courseID = $_POST['courseID'];
+    $grade = $_POST['grade'];
+    $grade = (int)$grade;
+
+    global $message;
+    $status = "success";
+    $pass = true;
+
+    if (strtoupper($courseID) != 'CSCI400' && strtoupper($courseID) != 'CSCI343') {
+        $message = "Invalid course ID";
+        $status = "error";
+        $pass = false;
+    }
+
+    if (($grade < 0) || ($grade > 100)) {
+        $message = "Invalid grade";
+        $status = "error";
+        $pass = false;
+    }
+
+    if (strtoupper($courseID) == 'CSCI400' && $grade < 70) {
+        $message = "You have failed CSCI400";
+        $status = "fail";
+        $pass = false;
+    }
+
+    if (strtoupper($courseID) == 'CSCI343' && $grade < 75) {
+        $message = "You have failed CSCI343";
+        $status = "fail";
+        $pass = false;
+    }
+
+    if ($pass) {
+        $message = "You have successfully PASSED $courseID with a grade of $grade!";
+    }
+
+
+    $response = array(
+        "courseID" => $courseID,
+        "grade" => $grade,
+        "message" => $message,
+        "status" => $status,
+        "pass" => $pass
+    );
+    echo json_encode($response);
+}
+
+
+
+
+//if method is post, form input
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    processFormJSON();
+}
